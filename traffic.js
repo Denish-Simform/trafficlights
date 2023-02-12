a8c2641740d2b9c8c88ff891c99fb1b8c5c003df
Denish Bhimani <denish@simformsolutions.com>
2023-02-12 15:45:25 +0530

    Finale push

--- traffic.js
+++ traffic.js
@@ -1,8 +1,8 @@
-var global_timer, total_time = 79;
+var global_timer, total_time = 60, go = 1;
 
 let global = function(time) {
     global_timer = setInterval(function (){
-        if(time == 0) {
+        if(time == -20) {
             time = total_time;
         }
         position_timer(time,'top');
@@ -16,43 +16,33 @@ let global = function(time) {
 function position_timer (time,position) {
     let flag;
     let position1 = position;
-    if (time < 0) {
-        time = 80 + time;
-        if (time < 41 && time > 20) {
-            time = time - 20;
-            flag = 1;
-            light_logic(time,position1,flag);
-            document.getElementById('timer-'+position1).innerText = time;
-        } else {
-            flag = 0;
-            light_logic(time,position1,flag);
-            document.getElementById('timer-'+position1).innerText = time;
-        }
-    } else if (time < 41 && time > 20) {
-        time = time - 20;
+    if (time <= 0 && time > -21) {
+        time = 20 + time;
         flag = 1;
         light_logic(time,position1,flag);
         document.getElementById('timer-'+position1).innerText = time;
-    } else {
+    } else if (time <= -21) {
+        time = 80 + time;
         flag = 0;
         light_logic(time,position1,flag);
-        document.getElementById('timer-'+position1).innerText = time; //80
+        document.getElementById('timer-'+position1).innerText = time;
+    } 
+    else {
+        flag = 0;
+        light_logic(time,position1,flag);
+        document.getElementById('timer-'+position1).innerText = time;
     }
 }
 
 function light_logic(time,position,flag) {
-    if (time < 5 && flag == 0) {
-        document.getElementById(position+"-yellow").style.backgroundColor = 'yellow';
+    if (time < 5 && flag == 1 && time > 0) {
+        document.getElementById(position+"-yellow").style.backgroundColor = 'orange';
         document.getElementById(position+"-green").style.backgroundColor = 'black';
         document.getElementById(position+"-red").style.backgroundColor = 'black';
-    } else if(time < 21 && time > 4 && flag == 0) {
+    } else if (flag == 1) {
+        document.getElementById(position+"-yellow").style.backgroundColor = 'black';
         document.getElementById(position+"-green").style.backgroundColor = 'green';
-        document.getElementById(position+"-yellow").style.backgroundColor = 'black';
         document.getElementById(position+"-red").style.backgroundColor = 'black';
-    } else if(time < 21 && time > 4 && flag == 1) {
-        document.getElementById(position+"-green").style.backgroundColor = 'black';
-        document.getElementById(position+"-yellow").style.backgroundColor = 'black';
-        document.getElementById(position+"-red").style.backgroundColor = 'red';
     } else {
         document.getElementById(position+"-red").style.backgroundColor = 'red';
         document.getElementById(position+"-yellow").style.backgroundColor = 'black';
